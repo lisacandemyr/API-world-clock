1716d26fc6c7a2d72818d1262455b4bcfa635e3d
lisacandemyr <141151710+lisacandemyr@users.noreply.github.com>
2023-09-21 19:19:13 +0100

    added dropdown menu with alerts

--- app.js
+++ app.js
@@ -12,3 +12,32 @@ function updateClock() {
 updateClock();
 
 setInterval(updateClock, 1000);
+
+function alertCity(event) {
+  if (event.target.value.length > 0) {
+    if (event.target.value === "paris") {
+      alert(
+        `It is ${moment()
+          .tz("Europe/Paris")
+          .format("dddd, MMMM D, YYYY h:m A")} in Europe/Paris`
+      );
+    }
+    if (event.target.value === "tokyo") {
+      alert(
+        `It is ${moment()
+          .tz("Asia/Tokyo")
+          .format("dddd, MMMM D, YYYY h:m A")} in Asia/Tokyo`
+      );
+    }
+    if (event.target.value === "sydney") {
+      alert(
+        `It is ${moment()
+          .tz("Australia/Sydney")
+          .format("dddd, MMMM D, YYYY h:m A")} in Australia/Sydney`
+      );
+    }
+  }
+}
+
+let city = document.querySelector("#city");
+city.addEventListener("change", alertCity);
